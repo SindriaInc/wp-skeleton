02228682b90e0de2f4d717526791e9455782d9c6
lucapitzoi <luca.pitzoi@sindria.org>
2020-04-19 19:47:46 +0200

    Added autoload psr-4

--- index.php
+++ index.php
@@ -15,3 +15,6 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-core/wp-blog-header.php';
+
+/** Autoload composer vendor */
+require_once __DIR__ . 'vendor/autoload.php';
